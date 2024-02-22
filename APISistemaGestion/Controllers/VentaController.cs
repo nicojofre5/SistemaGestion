7be4309b07e1919c81db5bb88582fad23ade907e
Nicolas <lnicolas.jofre@outlook.com>
2024-02-22 14:23:44 -0300

    Cambios en controller

---
+++ APISistemaGestion/Controllers/VentaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APISistemaGestion.Controllers
+{
+    public class VentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
