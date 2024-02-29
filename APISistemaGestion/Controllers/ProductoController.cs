976407c69d4676983372a07a4c371a9750ce490a
Nicolas <lnicolas.jofre@outlook.com>
2024-02-29 01:09:44 -0300

    EndPoint controller producto

--- APISistemaGestion/Controllers/ProductoController.cs
+++ APISistemaGestion/Controllers/ProductoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaGestionBussiness;
+using SistemaGestionEntities.models;
 
 namespace APISistemaGestion.Controllers
 {
@@ -6,6 +8,11 @@ namespace APISistemaGestion.Controllers
     [Route("api/[controller]")]
     public class ProductoController : Controller
     {
-        
+        [HttpGet]
+
+        public ActionResult<List<Producto>> GetUsuarios()
+        {
+            return ProductoService.ListarProductos();
+        }
     }
 }
